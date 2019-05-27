de0d6a359d439231f46b7b2c0082359486ddeb63
Mateusz Chojnacki <mateuszchojnacki@Mateuszs-MacBook-Pro.local>
2019-05-27 22:29:02 +0200

    firebase auth basics

---
+++ auth/src/components/common/Header.js
@@ -0,0 +1,37 @@
+// Import Librares for making a component
+import React from 'react';
+import { Text, View } from 'react-native';
+
+//Make a component
+
+const Header = (props) => {
+    const { viewStyle,textStyle } = styles;
+
+    return (
+        <View style={viewStyle}>
+            <Text style={textStyle}>{props.headerText}</Text>
+        </View>
+    );
+};
+
+const styles = {
+
+    viewStyle: {
+      backgroundColor: '#abc',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 60,
+        paddingTop: 30,
+        shadowColor: '#000',
+        shadowOffset: {width: 0, height: 5},
+        shadowOpacity: 0.4,
+        elevation: 2,
+        position: 'relative'
+    },
+    textStyle: {
+        fontSize: 20
+    }
+};
+// Make the component avaiable to other part of the apps
+export { Header };
+
